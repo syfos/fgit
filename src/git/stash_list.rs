cfac06170c947014a177ae147f844ccf7bb44702
pudep <261843105+pudep@users.noreply.github.com>
2026-08-09 20:35:07 +0530

    feat: add `StashData` return type for `Git::get_stash_list`.

--- src/git/stash_list.rs
+++ src/git/stash_list.rs
@@ -1,16 +1,33 @@
+use std::result;
 use crate::git::Git;
-use git2::Repository;
+use git2::{Oid, Repository};
+
+#[allow(dead_code)]
+pub struct StashData {
+  pub index: usize,
+  pub message: String,
+  pub oid: Oid,
+}
 
 #[allow(dead_code)]
 impl Git {
-  // Gives vector containing tuple of stash's index, message, and oid.
-  pub fn get_stash_list(
-    repo: &mut Repository,
-  ) -> anyhow::Result<Vec<(usize, String, git2::Oid)>, git2::Error> {
+  /// Returns [`StashData`]. Chances of error: `rare`.
+  /// ```
+  /// pub struct StashData {
+  ///   pub index: usize,
+  ///   pub message: String,
+  ///   pub oid: Oid,
+  /// }
+  /// ```
+  pub fn get_stash_list(repo: &mut Repository) -> result::Result<Vec<StashData>, git2::Error> {
     let mut stashes = Vec::new();
 
     repo.stash_foreach(|index, message, oid| {
-      stashes.push((index, message.to_string(), *oid));
+      stashes.push(StashData {
+        index,
+        message: message.to_string(),
+        oid: *oid,
+      });
       true // return true to keep iterating, false to stop early
     })?;
 
