a63b7029ac899546f365bbe0e4d560709f464220
pudep <261843105+pudep@users.noreply.github.com>
2026-08-09 20:07:10 +0530

    docs: add explanatory doc comments.

--- src/git/commit_log.rs
+++ src/git/commit_log.rs
@@ -1,10 +1,14 @@
+use std::result;
+
 use crate::git::Git;
 use git2::{Oid, Repository, Sort};
 
 #[allow(dead_code)]
 impl Git {
-  /// Get the entire commit log of `repo`.
-  pub fn get_commits_log(repo: &Repository) -> anyhow::Result<Vec<Oid>, git2::Error> {
+  /// Returns `list` of `local commits` present in repo.
+  /// List order will always be sorted `topological` (`child commit` before `parent`) along with `time`(`newest` before `oldest`).
+  /// Chance of faliure is rare.
+  pub fn get_present_commits_list(repo: &Repository) -> result::Result<Vec<Oid>, git2::Error> {
     // Walk the commit graph.
     let mut revwalk = repo.revwalk()?;
     // use the head'oid as seed to the iterator.
