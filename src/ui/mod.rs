7ab2630b38e0d8777164ad1e5e33b548c1e46da3
pudep <261843105+pudep@users.noreply.github.com>
2026-08-11 18:44:36 +0530

    feat: Add simple horizontal split logic.

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,15 +1,57 @@
-use ratatui::layout::Rect;
+use ratatui::{
+  DefaultTerminal,
+  layout::{Constraint, Rect},
+  widgets::{Block, Borders, Paragraph},
+};
+use std::{error::Error, result::Result};
 
 pub struct Buffer {
   component: Vec<Rect>,
-  uid: BufUid,
 }
 
-pub struct BufUid;
-impl BufUid {
-  fn assign() -> Self {
-    Self {
-      
+pub struct Ui {
+  pub buffer: Buffer,
+}
+
+impl Ui {
+  pub fn new() -> Self {
+    Self {}
+  }
+  pub fn run() -> Result<(), Box<dyn Error>> {
+    ratatui::run(|terminal| Self::draw(terminal))?;
+    Ok(())
+  }
+
+  fn split_horizontal(area: Rect) -> Vec<Rect> {
+    let constraints = vec![Constraint::Ratio(1, 1)];
+    ratatui::layout::Layout::default()
+      .direction(ratatui::layout::Direction::Vertical)
+      .constraints(constraints)
+      .split(area)
+      .to_vec()
+  }
+
+  fn draw(terminal: &mut DefaultTerminal) -> std::result::Result<(), Box<dyn Error>> {
+    loop {
+      terminal.draw(|frame| {
+        let chunks = Self::split_horizontal(frame.area());
+        for (i, chunk) in chunks.iter().enumerate() {
+          let borders = if i == 0 {
+            Borders::TOP | Borders::BOTTOM
+          } else {
+            Borders::BOTTOM
+          };
+
+          let block = Block::default().borders(borders);
+
+          let paragraph = Paragraph::new(format!("Panel: {i}")).block(block);
+
+          frame.render_widget(paragraph, *chunk);
+        }
+      })?;
+
+
+      if let 
     }
   }
 }
