b7b8b9dbad765d61cb5f0db5e3392230a9eb7eba
pudep <261843105+pudep@users.noreply.github.com>
2026-08-11 19:14:03 +0530

    refactor: remove redundant keymaps.

--- src/keys/input.rs
+++ src/keys/input.rs
@@ -1,17 +1,11 @@
-use crate::{
-  action::navigate::Action,
-  tui::{App, Pages},
-};
+use crate::action::Action;
+
 use crossterm::event::{Event, KeyCode, KeyModifiers};
 use std::error::Error;
-pub fn handle_input(app: &mut App) -> std::result::Result<Action, Box<dyn Error>> {
+pub fn handle_input() -> std::result::Result<Action, Box<dyn Error>> {
   if let Event::Key(key) = crossterm::event::read()? {
     match key.code {
-      KeyCode::Up if app.active_page == Pages::HomePage => {}
-      KeyCode::Down if app.active_page == Pages::HomePage => {}
       KeyCode::Char('q') if key.modifiers == KeyModifiers::CONTROL => return Ok(Action::Quit),
-
-      KeyCode::Char('?') => return Ok(Action::GoToHelpPage),
 
       KeyCode::Char('h') => return Ok(Action::GoToHomePage),
       _ => {}
