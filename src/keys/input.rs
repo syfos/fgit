f9b9eef7b801cceb79086fc0f4462edf53748c2b
pudep <261843105+pudep@users.noreply.github.com>
2026-08-12 07:47:39 +0530

    refactor: change return type of `handle_input()` and make it a `implementation` of `App`.

--- src/keys/input.rs
+++ src/keys/input.rs
@@ -1,15 +1,18 @@
-use crate::action::Action;
-
 use crossterm::event::{Event, KeyCode, KeyModifiers};
 use std::error::Error;
-pub fn handle_input() -> std::result::Result<Action, Box<dyn Error>> {
-  if let Event::Key(key) = crossterm::event::read()? {
-    match key.code {
-      KeyCode::Char('q') if key.modifiers == KeyModifiers::CONTROL => return Ok(Action::Quit),
+use crate::{action::InputSignal, app::App};
 
-      KeyCode::Char('h') => return Ok(Action::GoToHomePage),
-      _ => {}
+impl App {
+  pub fn handle_input() -> std::result::Result<InputSignal, Box<dyn Error>> {
+    if let Event::Key(key) = crossterm::event::read()? {
+      match key.code {
+        KeyCode::Char('q') if key.modifiers == KeyModifiers::CONTROL => {
+          return Ok(InputSignal::Quit);
+        }
+
+        _ => {}
+      }
     }
+    Ok(InputSignal::None)
   }
-  Ok(Action::None)
 }
