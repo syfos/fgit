fbc0f09beb590d531ab49aab4330083c7966709b
pudep <261843105+pudep@users.noreply.github.com>
2026-08-12 08:32:51 +0530

    feat: add `EventManager`

--- src/action/mod.rs
+++ src/action/mod.rs
@@ -1,5 +1,37 @@
+use crossterm::event::KeyCode;
+#[allow(dead_code)]
+/// Event manager for `Fgit`.
+pub struct EventManager {
+  mode: ModeType,
+  key_pressed: IoSignal,
+  pending_prefix: KeyCode,
+}
+
+impl EventManager {
+  pub fn new() -> Self {
+    Self {
+      mode: ModeType::Normal,
+      key_pressed: IoSignal::None,
+      pending_prefix: KeyCode::Null,
+    }
+  }
+}
+
+impl Default for EventManager {
+  fn default() -> Self {
+    Self::new()
+  }
+}
+
 #[allow(dead_code)]
 pub enum IoSignal {
   Quit,
   None,
+}
+
+#[allow(dead_code)]
+enum ModeType {
+  Normal,
+  Visual,
+  Insert,
 }
