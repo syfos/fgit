ed49d4c31837c4c3f3c1134f2bc06f5f31d67e53
pudep <261843105+pudep@users.noreply.github.com>
2026-08-13 14:52:54 +0530

    feat: add `<C-w>s` sequence detection.

--- src/keys/input.rs
+++ src/keys/input.rs
@@ -1,16 +1,23 @@
+use crate::{action::IoSignal, app::App};
 use crossterm::event::{Event, KeyCode, KeyModifiers};
 use std::error::Error;
-use crate::{action::IoSignal, app::App};
 
 impl App {
-  pub fn handle_input() -> std::result::Result<IoSignal, Box<dyn Error>> {
+  pub fn handle_input(&mut self) -> std::result::Result<IoSignal, Box<dyn Error>> {
     if let Event::Key(key) = crossterm::event::read()? {
       match key.code {
         KeyCode::Char('q') if key.modifiers == KeyModifiers::CONTROL => {
           return Ok(IoSignal::Quit);
         }
 
-
+        KeyCode::Char('w') if key.modifiers == KeyModifiers::CONTROL => {
+          if let Event::Key(next) = crossterm::event::read()? {
+            match next.code {
+              KeyCode::Char('s') => return Ok(IoSignal::Split),
+              _ => {}
+            }
+          }
+        }
 
         _ => {}
       }
