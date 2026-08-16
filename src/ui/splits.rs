ccd461f3358f7b6e73d08d1b6eab38cbc55ce885
pudep <261843105+pudep@users.noreply.github.com>
2026-08-16 09:58:42 +0530

    refactor(ui): remove dupe methods and unneeded field from struct Split in "ui/splits.rs".
    Why: Previously there was too dupe code, methods relied on "&mut Tui" instead of "&Splits".
    
    Changes:
    
    1. SplitAxis and SplitSeperator are introduced.
    2. Single methods to preform dual work depending on tye of Parameter.
    3. Max usage of "&self" and "&mut self".

--- src/ui/splits.rs
+++ src/ui/splits.rs
@@ -1,107 +1,65 @@
 use ratatui::{
-   Frame,
-  layout::{Constraint, Rect},
+  Frame,
+  layout::{Constraint, Direction, Rect},
   widgets::{Block, Borders, Paragraph},
 };
 
-use crate::ui::Tui;
-/// Contains everything regarding splits.
+/// Contains splits based on two directions `Horizontal` and `Vertical`.
+#[derive(Default)]
 pub struct Splits {
-  pub vsplit_count: u32,
-  pub hsplit_count: u32,
-  pub is_render_vertical: bool,
-  pub is_render_horizontal: bool,
-  pub vsplits: Vec<Rect>,
-  pub hsplits: Vec<Rect>,
+  pub horizontal: SplitAxis,
+  pub vertical: SplitAxis,
 }
 
-impl Splits {
-  /// Generates default state of splits for the given `tui.buf_area` i.e `current buffer area`
-  ///
-  ///```
-  /// // returns
-  ///Self {
-  ///  vsplit_count: 0,
-  ///  hsplit_count: 0,
-  ///  is_render_vertical: false,
-  ///  is_render_horizontal: false,
-  ///  vsplits: Vec::new(),
-  ///  hsplits: Vec::new(),
-  ///}
-  ///```
-  pub fn new() -> Self {
-    Self {
-      vsplit_count: 0,
-      hsplit_count: 0,
-      is_render_vertical: false,
-      is_render_horizontal: false,
-      vsplits: Vec::new(),
-      hsplits: Vec::new(),
+/// Pick `Bottom` for rendering `Vertical` splits and `Right` for rendering `Horizontal` splits.
+pub enum SplitSeperator {
+  Bottom,
+  Right,
+}
+
+impl SplitSeperator {
+  /// Returns the equivalent [`Borders`] bitflag.
+  /// ```
+  /// match self {
+  ///   SplitSeperator::Bottom => Borders::BOTTOM,
+  ///   SplitSeperator::Right => Borders::RIGHT,
+  /// }
+  /// ```
+  fn as_borders(&self) -> Borders {
+    match self {
+      SplitSeperator::Bottom => Borders::BOTTOM,
+      SplitSeperator::Right => Borders::RIGHT,
     }
   }
+}
 
-  pub fn increment_vsplit_count(&mut self) {
-    self.vsplit_count = self.vsplit_count.saturating_add(1);
+/// Contains count and splits for two underlying directions `Horizontal` and `Vertical`.
+#[derive(Default)]
+pub struct SplitAxis {
+  pub count: u32,
+  pub splits: Vec<Rect>,
+}
+
+impl SplitAxis {
+  pub fn increment_count(&mut self) {
+    self.count += self.count.saturating_add(1);
   }
 
-  pub fn increment_hsplit_count(&mut self) {
-    self.hsplit_count = self.hsplit_count.saturating_add(1);
-  }
-
-  pub fn update_vsplits(&mut self, buf_area: Rect) {
-    self.vsplits = self.split_vertically(buf_area);
-  }
-
-  pub fn update_hsplits(&mut self, buf_area: Rect) {
-    self.hsplits = self.split_horizontally(buf_area);
-  }
-  /// Produces equal splits in `vertical direction`, i.e  `stacked one above another`, by dividing the entire terminal area by `net_vsplits`.
-  fn split_vertically(&self, buf_area: Rect) -> Vec<Rect> {
-    let mut x = self.vsplit_count;
-
-    // The net_vsplits is just a counter
-    x += 1;
-    // vec![value; count];
-    // the number of chunks you get out of Layout::split() always equals the number of constraints you put in.
-    // Hence so the size of vector == net_split
+  pub fn split(&mut self, buf_area: Rect, direction: Direction) {
+    let x = self.count + 1;
     let constraints = vec![Constraint::Ratio(1, x); x as usize];
-    ratatui::layout::Layout::default()
-      .direction(ratatui::layout::Direction::Vertical)
+    self.splits = ratatui::layout::Layout::default()
+      .direction(direction)
       .constraints(constraints)
       .split(buf_area)
       .to_vec()
   }
 
-  /// Produces equal splits in `horizontal direction`, i.e  `side by side`, by dividing the entire terminal area by `net_hsplits`.
-  fn split_horizontally(&self, buf_area: Rect) -> Vec<Rect> {
-    let mut x = self.hsplit_count;
-    x += 1;
-    let constraints = vec![Constraint::Ratio(1, x); x as usize];
-    ratatui::layout::Layout::default()
-      .direction(ratatui::layout::Direction::Horizontal)
-      .constraints(constraints)
-      .split(buf_area)
-      .to_vec()
-  }
-  // Handles rendering logic for splits.
-  pub fn render_vsplits(tui: &Tui, frame: &mut Frame) {
-    let v_len = tui.splits.vsplits.len();
-    for (i, chunk) in tui.splits.vsplits.iter().enumerate() {
-      let borders = if i + 1 < v_len {
-        Borders::BOTTOM
-      } else {
-        Borders::NONE
-      };
-      let block = Block::default().borders(borders);
-      let paragraph = Paragraph::new(format!("Panel: {i}")).block(block);
-      frame.render_widget(paragraph, *chunk);
-    }
-  }
-  pub fn render_hsplits(tui: &Tui, frame: &mut Frame) {
-    let h_len = tui.splits.hsplits.len();
-    for (i, chunk) in tui.splits.hsplits.iter().enumerate() {
-      let borders = if i + 1 < h_len {
-        Borders::RIGHT
+  pub fn render(&self, frame: &mut Frame, seperator: SplitSeperator) {
+    let seperator = seperator.as_borders();
+    for (i, chunk) in self.splits.iter().enumerate() {
+      let borders = if (i + 1) < self.splits.len() {
+        seperator
       } else {
         Borders::NONE
       };
