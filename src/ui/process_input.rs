1f0d7017a474695c3cc841fc61b6ff7417fb734c
syfos <261843105+syfos@users.noreply.github.com>
2026-08-20 11:35:23 +0530

    feat(keys): handle motion signals and make each do a particular task

--- src/ui/process_input.rs
+++ src/ui/process_input.rs
@@ -2,10 +2,9 @@ use crate::{action::IoSignal, ui::Tui};
 use std::error::Error;
 impl Tui {
   pub fn process_input(&mut self) -> std::result::Result<bool, Box<dyn Error>> {
-
     // Immediate mode tui calculates on every frame.
     // Never call draw logic here.
-    match Self::handle_input() {
+    match Self::handle_input(self) {
       Ok(IoSignal::Quit) => Ok(true),
 
       Ok(IoSignal::Vsplit) => {
@@ -26,6 +25,24 @@ impl Tui {
       Ok(IoSignal::DelHsplit) => {
         self.splits.horizontal.decrement_count();
         self.splits.horizontal.del_split();
+        Ok(false)
+      }
+
+      Ok(IoSignal::Up) => {
+        self.cursor_row = self.cursor_row.saturating_sub(1);
+        Ok(false)
+      }
+      Ok(IoSignal::Down) => {
+        self.cursor_row = self.cursor_row.saturating_add(1);
+        Ok(false)
+      }
+      Ok(IoSignal::Left) => {
+        self.cursor_col = self.cursor_col.saturating_sub(1);
+
+        Ok(false)
+      }
+      Ok(IoSignal::Right) => {
+        self.cursor_col = self.cursor_col.saturating_add(1);
         Ok(false)
       }
 
