c60f6da82c80b08316fbd1f75fc2db95351ca158
syfos <261843105+syfos@users.noreply.github.com>
2026-08-21 18:20:11 +0530

    refactor(process_input): update access of cursor fields.

--- src/ui/process_input.rs
+++ src/ui/process_input.rs
@@ -29,20 +29,20 @@ impl Tui {
       }
 
       Ok(IoSignal::Up) => {
-        self.editor.cursor_row = self.editor.cursor_row.saturating_sub(1);
+        self.editor.cursor.0 = self.editor.cursor.0.saturating_sub(1);
         Ok(false)
       }
       Ok(IoSignal::Down) => {
-        self.editor.cursor_row = self.editor.cursor_row.saturating_add(1);
+        self.editor.cursor.0 = self.editor.cursor.0.saturating_add(1);
         Ok(false)
       }
       Ok(IoSignal::Left) => {
-        self.editor.cursor_col = self.editor.cursor_col.saturating_sub(1);
+        self.editor.cursor.1 = self.editor.cursor.1.saturating_sub(1);
 
         Ok(false)
       }
       Ok(IoSignal::Right) => {
-        self.editor.cursor_col = self.editor.cursor_col.saturating_add(1);
+        self.editor.cursor.1 = self.editor.cursor.1.saturating_add(1);
         Ok(false)
       }
 
