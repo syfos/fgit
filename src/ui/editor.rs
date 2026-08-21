288fb266764beddc1cf7043a7853ae4629dbc282
syfos <261843105+syfos@users.noreply.github.com>
2026-08-21 18:11:08 +0530

    feat(editor): add Editor::push_char

--- src/ui/editor.rs
+++ src/ui/editor.rs
@@ -10,3 +10,9 @@ pub struct Line(pub Vec<char>);
 /// The cursor position in `(row, col)` format.
 #[derive(Default, Clone, Copy)]
 pub struct Cursor(pub usize, pub usize);
+
+impl Editor {
+  pub fn push_char(&mut self, char: char) {
+    self.line.0.insert(self.cursor.1 as usize, char);
+  }
+}
