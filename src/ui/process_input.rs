6c2066929bdba598cf102560f1c8a3ec720b4b10
syfos <261843105+syfos@users.noreply.github.com>
2026-08-22 16:18:35 +0530

    fix(process_input): use Editor::increment_col, Editor::decrement_col, Editor::increment_row and Editor::decrement_row to avoid invalid column access

--- src/ui/process_input.rs
+++ src/ui/process_input.rs
@@ -29,22 +29,22 @@ impl Tui {
       }
 
       Ok(IoSignal::Up) => {
-        self.editor.cursor.1 = self.editor.cursor.1.saturating_sub(1);
+        self.editor.decrement_row_by(1);
         Ok(false)
       }
 
       Ok(IoSignal::Down) => {
-        self.editor.cursor.1 = self.editor.cursor.1.saturating_add(1);
+        self.editor.increment_row_by(1);
         Ok(false)
       }
 
       Ok(IoSignal::Left) => {
-        self.editor.cursor.0 = self.editor.cursor.0.saturating_sub(1);
+        self.editor.decrement_col_by(1);
         Ok(false)
       }
 
       Ok(IoSignal::Right) => {
-        self.editor.cursor.0 = self.editor.cursor.0.saturating_add(1);
+        self.editor.increment_col_by(1);
         Ok(false)
       }
 
