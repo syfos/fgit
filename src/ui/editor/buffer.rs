9146071fbbcfaba348eb22687ccad57ed5fc3ae5
syfos <261843105+syfos@users.noreply.github.com>
2026-08-23 21:06:41 +0530

    feat(buffer): add Buffer and src/ui/editor/buffer.rs

---
+++ src/ui/editor/buffer.rs
@@ -0,0 +1,10 @@
+use crate::ui::editor::Editor;
+
+#[allow(dead_code)]
+impl Editor {
+  fn iter(&mut self) {
+    for (idx, value) in self.buffer.0.iter().enumerate() {
+
+    }
+  }
+}
--- src/ui/editor/mod.rs
+++ src/ui/editor/mod.rs
@@ -1,14 +1,21 @@
+pub mod buffer;
 pub mod cursor;
 
 #[derive(Default)]
 pub struct Editor {
+  pub buffer: Buffer,
   pub line: Line,
   /// current position of cursor in `(col, row)` format
   pub cursor: Cursor,
 }
 
+/// Lines create a [`Buffer`].
 #[derive(Default)]
 pub struct Line(pub Vec<char>);
+
+/// Buffer is a collection of [`Line`].
+#[derive(Default)]
+pub struct Buffer(pub Vec<Line>);
 
 /// The cursor position in `(col, row)` format.
 #[derive(Default, Clone, Copy)]
