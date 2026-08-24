576347fd8b243afa4230d1395a46bd077d468cdc
syfos <261843105+syfos@users.noreply.github.com>
2026-08-24 14:57:15 +0530

    feat(buffer): add method Editor::insert

--- src/ui/editor/buffer.rs
+++ src/ui/editor/buffer.rs
@@ -1,3 +1,7 @@
+use ratatui::style::Color;
+use ratatui::style::Style;
+use ratatui::{Frame, layout::Rect};
+
 use crate::ui::editor::{Editor, Line};
 
 #[allow(dead_code)]
@@ -7,4 +11,11 @@ impl Editor {
     self.buffer.0.push(Line::default());
     self.increment_cursor_row_by(1);
   }
+
+  /// Inserts [`char`] at specific index of cursor line.
+  pub fn insert(&mut self, idx: usize, char: char) {
+    let line = self.mut_ref_cursor_line();
+    line.insert(idx, char);
+  }
+
 }
