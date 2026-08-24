b0208cfb52740d2c6aab95b630fd5503303a95bf
syfos <261843105+syfos@users.noreply.github.com>
2026-08-24 08:43:38 +0530

    feat(buffer): make cursor move to next line on enter

--- src/ui/editor/buffer.rs
+++ src/ui/editor/buffer.rs
@@ -4,6 +4,7 @@ use crate::ui::editor::{Editor, Line};
 impl Editor {
   pub fn create_new_line(&mut self) {
     self.buffer.0.push(Line::default());
+    self.increment_row_by(1);
   }
   fn iter(&mut self) {
     for (idx, value) in self.buffer.0.iter().enumerate() {
