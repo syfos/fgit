832ae5f8a838190f79c7ed7c99aecf7a1ef5054e
syfos <261843105+syfos@users.noreply.github.com>
2026-08-24 09:59:55 +0530

    refactor(buffer): remove unneeded Editor::iter

--- src/ui/editor/buffer.rs
+++ src/ui/editor/buffer.rs
@@ -6,9 +6,4 @@ impl Editor {
     self.buffer.0.push(Line::default());
     self.increment_row_by(1);
   }
-  fn iter(&mut self) {
-    for (idx, value) in self.buffer.0.iter().enumerate() {
-
-    }
-  }
 }
