147911954f434b1a2f91417ba26ec0814c6c012f
syfos <261843105+syfos@users.noreply.github.com>
2026-08-24 08:40:58 +0530

    feat(buffer): add Editor::create_new_line

--- src/ui/editor/buffer.rs
+++ src/ui/editor/buffer.rs
@@ -1,7 +1,10 @@
-use crate::ui::editor::Editor;
+use crate::ui::editor::{Editor, Line};
 
 #[allow(dead_code)]
 impl Editor {
+  pub fn create_new_line(&mut self) {
+    self.buffer.0.push(Line::default());
+  }
   fn iter(&mut self) {
     for (idx, value) in self.buffer.0.iter().enumerate() {
 
