23a5b97ef75f85c46251f82950300058102f3fc1
syfos <261843105+syfos@users.noreply.github.com>
2026-08-25 18:39:16 +0530

    chore: add src/bin and src/bin/txt.txt

---
+++ src/bin/bin.rs
@@ -0,0 +1,25 @@
+use anyhow::Context;
+use ropey::Rope;
+use std::path::{Path, PathBuf};
+use std::{fs, io::BufReader};
+
+fn string_to_path(path_string: &str) -> anyhow::Result<PathBuf> {
+  let expanded = shellexpand::full(path_string)
+    .with_context(|| format!("failed to expand path: `{path_string}`"))?;
+
+  let canonical = Path::new(expanded.as_ref())
+      .canonicalize()
+      .with_context(|| format!("The expanded path do not exists or is inaccessible : `{expanded}` probably the `{path_string}` is wrong."))?;
+  Ok(canonical)
+}
+
+fn main() -> anyhow::Result<()> {
+  let rope = Rope::from_reader(BufReader::new(fs::File::open(string_to_path(
+    "~/impl/rust/fgit/src/bin/txt.txt",
+  )?)?))?;
+  let char_len = rope.line(0).len_chars();
+  println!("Char len: {char_len}");
+  println!("Line: {:?}",rope.line(0).to_string());
+  // println!("Line: {:?}",rope.line(1).to_string());
+  Ok(())
+}
