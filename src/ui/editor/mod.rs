045be2432ad147bc4406c127efd637264c63f32c
syfos <261843105+syfos@users.noreply.github.com>
2026-08-25 18:33:55 +0530

    feat(editor)!: remove structs Line and Buffer

--- src/ui/editor/mod.rs
+++ src/ui/editor/mod.rs
@@ -1,26 +1,14 @@
 use ropey::Rope;
 
-pub mod buffer;
 pub mod cursor;
 pub mod edit;
 
 #[derive(Default)]
 pub struct Editor {
-  pub buffer: Buffer,
-  pub line: Line,
   pub rope: Rope,
   /// current position of cursor in `(col, row)` format
   pub cursor: Cursor,
 }
-
-/// Lines create a [`Buffer`].
-#[derive(Default)]
-pub struct Line(pub Vec<char>);
-
-/// Buffer is a collection of [`Line`].
-#[derive(Default)]
-pub struct Buffer(pub Vec<Line>);
-
 /// The cursor position in `(col, row)` format.
 #[derive(Default, Clone, Copy)]
 pub struct Cursor(pub usize, pub usize);
