7a440900af531b857a9c3a928dbd2753093adb09
syfos <261843105+syfos@users.noreply.github.com>
2026-08-25 18:30:06 +0530

    refactor(ui): remove unused fields from Tui

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,32 +1,25 @@
-use crate::{
-  action::EventManager,
-  ui::{buffer::ParentBuf, editor::Editor, splits::Splits},
-};
+use crate::ui::{editor::Editor, splits::Splits};
 use ratatui::layout::Rect;
 
 pub mod buffer;
+pub mod editor;
 pub mod keys;
 pub mod process_input;
 pub mod renderer;
 pub mod splits;
-pub mod editor;
 
 /// Tui module of Fgit.
 pub struct Tui {
-  pub parent_buf: ParentBuf,
   pub screen_area: Rect,
   pub splits: Splits,
-  pub event_manager: EventManager,
   pub editor: Editor,
 }
 
 impl Tui {
   pub fn new() -> Self {
     Self {
-      parent_buf: ParentBuf::GitHealth,
       screen_area: Rect::default(),
       splits: Splits::default(),
-      event_manager: EventManager::default(),
       editor: Editor::default(),
     }
   }
