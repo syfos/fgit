f5a619893ff6a114eaad5fc862ce14a293227bc4
syfos <261843105+syfos@users.noreply.github.com>
2026-08-25 18:41:38 +0530

    feat(editor): add Editor::insert_char Editor::remove_char and Editor::new_line to edit.rs

--- src/ui/editor/edit.rs
+++ src/ui/editor/edit.rs
@@ -1,8 +1,37 @@
+use ratatui::{
+  Frame,
+  layout::{Position, Rect},
+  text::{Line, Text},
+  widgets::Paragraph,
+};
+
 use crate::ui::editor::Editor;
 
 #[allow(dead_code)]
 impl Editor {
-  fn insert_char(&mut self){
+  pub fn insert_char(&mut self, char: char) {
+    let cursor_idx = self.rope.line_to_char(self.cursor.1) + self.cursor.0;
+    self.rope.insert_char(cursor_idx, char);
+    self.increment_cursor_col(1);
+  }
+
+  pub fn new_line(&mut self) {
+    self
+      .rope
+      .insert_char(self.rope.line(self.cursor.1).len_chars(), '\n');
+    self.increment_cursor_row(1);
+  }
+
+  pub fn remove_char(&mut self) {
+    let line = self.rope.line(self.cursor.1).to_string();
+    if line.is_empty() {
+      return;
+    }
+
+    let cursor_idx = self.rope.line_to_char(self.cursor.1) + self.cursor.0;
+    self.rope.remove(cursor_idx.saturating_sub(1)..cursor_idx);
+    self.decrement_cursor_row(1);
+  }
 
   }
 }
