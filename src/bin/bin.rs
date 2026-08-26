23eebed792102c52074c74b2ddf4e73d43305a7f
syfos <261843105+syfos@users.noreply.github.com>
2026-08-26 08:48:27 +0530

    test: cross check accuracy of Editor::new_line

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -17,15 +17,8 @@ fn main() -> anyhow::Result<()> {
   let mut rope = Rope::from_reader(BufReader::new(fs::File::open(string_to_path(
     "~/impl/rust/fgit/src/bin/txt.txt",
   )?)?))?;
-  let prev_line = rope.line(0);
-  println!("Line 0: {prev_line}");
-  let char_len = prev_line.len_chars();
-  println!("Line 0 char len: {char_len}");
 
-  let s = rope.line_to_char(0) + char_len;
-  println!("Char idx of last value of line 0: {char_len}");
-
-  rope.remove(s-1..s);
+  rope.insert_char(rope.line_to_char(0) + 4, '\n');
   println!("{rope:?}");
   Ok(())
 }
