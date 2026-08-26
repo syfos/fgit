363e7ba6b459483708ee81ec7f636dafb6cb1cd8
syfos <261843105+syfos@users.noreply.github.com>
2026-08-26 08:01:01 +0530

    test: cross check how line merge logic works

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -17,11 +17,15 @@ fn main() -> anyhow::Result<()> {
   let mut rope = Rope::from_reader(BufReader::new(fs::File::open(string_to_path(
     "~/impl/rust/fgit/src/bin/txt.txt",
   )?)?))?;
-  rope.insert_char(rope.line_to_char(0) + 4, '\n');
-  let lx1 = rope.line_to_char(0); 
-  let lx2 = rope.line_to_char(1); 
+  let prev_line = rope.line(0);
+  println!("Line 0: {prev_line}");
+  let char_len = prev_line.len_chars();
+  println!("Line 0 char len: {char_len}");
 
-  println!("{lx1}\n{lx2}");
-  // println!("{rope:?}");
+  let s = rope.line_to_char(0) + char_len;
+  println!("Char idx of last value of line 0: {char_len}");
+
+  rope.remove(s-1..s);
+  println!("{rope:?}");
   Ok(())
 }
