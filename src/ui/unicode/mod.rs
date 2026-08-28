97d8ec6c6e0bb209db813478f1a565aa10df74c8
syfos <261843105+syfos@users.noreply.github.com>
2026-08-28 09:21:40 +0530

    refactor(unicode): rename Unicode::get_grapheme_width to Unicode::get_grapheme_char_width

--- src/ui/unicode/mod.rs
+++ src/ui/unicode/mod.rs
@@ -10,7 +10,7 @@ impl Unicode {
   /// with char index of line.
   /// This give you flexibility to preform well
   /// cordinated unicode aware operations.
-  pub fn get_grapheme_width(line: &str) -> Vec<usize> {
+  pub fn get_grapheme_char_width(line: &str) -> Vec<usize> {
     let mut boundary = Vec::new();
     for grapheme in line.graphemes(true) {
       let grapheme_width = grapheme.chars().count();
