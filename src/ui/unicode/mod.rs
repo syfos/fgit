398a5a93458293ce0087df8cac8ffe6cc338fa80
syfos <261843105+syfos@users.noreply.github.com>
2026-08-29 15:32:05 +0530

    feat(unicode): add Unicode::check_canonical_form to check whether the string contains only NFC, only NFD, Both, or Neither of NFC or NFD

--- src/ui/unicode/mod.rs
+++ src/ui/unicode/mod.rs
@@ -1,9 +1,23 @@
+use ropey::Rope;
+use unicode_normalization::{is_nfc, is_nfd};
 use unicode_segmentation::UnicodeSegmentation;
 use unicode_width::UnicodeWidthStr;
+
+pub enum CanonicalType {
+  /// String contains `NFC` along `NFD`.
+  Mix,
+  /// String contains only `NFC`
+  Nfc,
+  /// String contains only `NFD`
+  Nfd,
+  /// String conatins neither of `NFC` or `NFD`
+  None,
+}
 
 #[allow(dead_code)]
 pub struct Unicode;
 
+#[allow(dead_code)]
 pub struct GraphemeLine {
   pub range: std::ops::Range<usize>,
   pub width: usize,
@@ -33,4 +47,33 @@ impl Unicode {
     }
     boundary
   }
+  /// This function returns the `[CanonicalType]` of Normalization form of the given string.
+  /// ```
+  /// use unicode_normalization::{is_nfc, is_nfd};
+  ///
+  /// match (is_nfc(query), is_nfd(query)) {
+  ///   // Means there is no NFC and NFD
+  ///   (true, true) => CanonicalType::None,
+  ///   // Means there is only NFC
+  ///   (true, false) => CanonicalType::Nfc,
+  ///   // Means there is only NFD
+  ///   (false, true) => CanonicalType::Nfd,
+  ///   // Means there is both
+  ///   (false, false) => CanonicalType::Mix,
+  /// }
+  /// ```
+  ///
+  pub fn check_canonical_form(query: &str) -> CanonicalType {
+    match (is_nfc(query), is_nfd(query)) {
+      // Means there is no NFC and NFD
+      (true, true) => CanonicalType::None,
+      // Means there is only NFC
+      (true, false) => CanonicalType::Nfc,
+      // Means there is only NFD
+      (false, true) => CanonicalType::Nfd,
+      // Means there is both
+      (false, false) => CanonicalType::Mix,
+    }
+  }
+
 }
