730ce173e5aa854dae8a22a9edb7d2de52b73564
syfos <261843105+syfos@users.noreply.github.com>
2026-08-29 11:05:05 +0530

    feat(unicode): add struct GraphemeLine and make Unicode::get_grapheme_ranges return GraphemeLine {range, width}

--- src/ui/unicode/mod.rs
+++ src/ui/unicode/mod.rs
@@ -3,6 +3,12 @@ use unicode_width::UnicodeWidthStr;
 
 #[allow(dead_code)]
 pub struct Unicode;
+
+pub struct GraphemeLine {
+  pub range: std::ops::Range<usize>,
+  pub width: usize,
+}
+
 #[allow(dead_code)]
 impl Unicode {
   /// Returns unicode aware character
@@ -11,7 +17,7 @@ impl Unicode {
   /// with char index of line.
   /// This give you flexibility to preform well
   /// cordinated unicode aware operations.
-  pub fn get_grapheme_ranges(line: &str, line_to_char: &usize) -> Vec<std::ops::Range<usize>> {
+  pub fn get_grapheme_ranges(line: &str, line_to_char: &usize) -> Vec<GraphemeLine> {
     let mut boundary = Vec::new();
     let mut offset = 0usize;
     for grapheme in line.graphemes(true) {
@@ -19,8 +25,11 @@ impl Unicode {
       let start = *line_to_char + offset;
       offset += grapheme_char_count;
       let end = *line_to_char + offset;
-      boundary.push(start..end);
       let grapheme_width = grapheme.width();
+      boundary.push(GraphemeLine {
+        range: start..end,
+        width: grapheme_width,
+      });
     }
     boundary
   }
