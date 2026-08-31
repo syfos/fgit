7ee4e6c3a037683c29e9ff117232f92ccb027b75
syfos <261843105+syfos@users.noreply.github.com>
2026-08-31 20:38:21 +0530

    chore(bin): delete test.rs

--- src/bin/test.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-  use unicode_segmentation::UnicodeSegmentation;
-
-  let rope_string = "A 👩‍💻 B é";
-
-  for (byte_idx, grapheme) in rope_string.grapheme_indices(true) {
-    println!("{byte_idx}: {grapheme:?}");
-  }
-}
