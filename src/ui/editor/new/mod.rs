2c7b0ed554bcd0ccf099eb75323678d5e8ec5cf5
syfos <261843105+syfos@users.noreply.github.com>
2026-09-01 15:06:50 +0530

    feat(new): add "Editor::get_viewport_lines"

--- src/ui/editor/new/mod.rs
+++ src/ui/editor/new/mod.rs
@@ -20,3 +20,30 @@ pub enum LineBreakChar {
   None,
 }
 
+#[allow(dead_code)]
+impl Editor {
+  /// Returns `Vec<String>` that will be displayed in the viewport of editor.
+  /// Note that each [`String`] of the vector is by default has only 1 valid line break.
+  pub fn get_viewport_lines(&mut self, viewport_height: &usize) -> Vec<ViewportLine> {
+    // The line_idx number of very first line to be rendered in viewport.
+    let mut scroll_offset = self.scroll_offset;
+
+    // A counter to track row along viewport_height
+    let mut row_tracker = 0usize;
+
+    let mut viewport_lines = Vec::new();
+
+    while row_tracker < *viewport_height + 1 {
+      let string = self.rope.line(scroll_offset).to_string();
+      let line_break_char = Self::detect_trailing_linebreak_char(&string);
+      viewport_lines.push(ViewportLine {
+        string,
+        line_break_char,
+      });
+      scroll_offset += 1;
+      row_tracker += 1;
+    }
+
+    viewport_lines
+  }
+}
